09140f286b3a0b81c7fd72da7ad4c2fb080de911
caixiang <caixiang@ualberta.ca>
2022-08-29 22:36:31 +0000

    added kernel upgrade commands

--- t1.sh
+++ t1.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# upgrade kernel to 5.10
+cd /tmp/
+
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-headers-5.10.0-051000_5.10.0-051000.202012132330_all.deb
+
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-headers-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-image-unsigned-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-modules-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+
+sudo dpkg -i *.deb
+
 # get firecraker binary
 release_url="https://github.com/firecracker-microvm/firecracker/releases"
 latest=$(basename $(curl -fsSLI -o /dev/null -w  %{url_effective} ${release_url}/latest))
